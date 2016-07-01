f365942f42b5f8d9cb14c7d07d733eb1a717f2ae
CIA <cgberlin>
2016-07-01 17:19:43 -0500

    Finished animations for both ryu and hulk ryu, all thats left is to tint the color of the hulk ryu

--- js/app.js
+++ js/app.js
@@ -8,15 +8,95 @@ $(document).ready(function() {
   	$('.ryu-still').show();
   })
   .mousedown(function(){
-  	//play hadouken sound
+  	playHadouken();
   	$('.ryu-ready').hide();
   	$('.ryu-throwing').show();
-  	$('.hadouken').show();
-  	//animate hadouken throw
+  	$('.hadouken').finish().show().animate(
+  		{'left':'1020px'},
+  		500,
+  		function(){
+  			$(this).hide();
+  			$(this).css('left', '550px');
+  		}
+  		);
   })
   .mouseup(function(){
   	$('.hadouken').hide();
   	$('.ryu-throwing').hide();
   	$('.ryu-ready').show();
   });
+  $(window).keydown(function (e) {
+  		if (e.keyCode == 88) {
+  			$('.ryu-still').hide();
+  			$('.ryu-ready').hide();
+  			$('.ryu-cool').show();
+  		}
+  })
+  .keyup(function (e) {
+  		if (e.keyCode == 88) {
+  			$('.ryu-cool').hide();
+  			$('.ryu-still').show();
+  		}
+
+  	});
+// hulk ryu below
+
+
+ $('.hulk-ryu').mouseenter(function() {
+    $('.hulk-still').hide();
+    $('.hulk-ready').show();
+  })
+  .mouseleave(function() {
+  	$('.hulk-ready').hide();
+  	$('.hulk-still').show();
+  })
+  .mousedown(function(){
+  	playHadouken();
+  	$('.hulk-ready').hide();
+  	$('.hulk-throwing').show();
+  	$('.hulk-hadouken').finish().show().animate(
+  		{'right':'900px'},
+  		500,
+  		function(){
+  			$(this).hide();
+  			$(this).css('right', '447px');
+  		}
+  		);
+  })
+  .mouseup(function(){
+  	$('.hulk-hadouken').hide();
+  	$('.hulk-throwing').hide();
+  	$('.hulk-ready').show();
+  });
+  $(window).keydown(function (e) {
+  		if (e.keyCode == 88) {
+  			$('.hulk-still').hide();
+  			$('.hulk-ready').hide();
+  			$('.hulk-cool').show();
+  		}
+  })
+  .keyup(function (e) {
+  		if (e.keyCode == 88) {
+  			$('.hulk-cool').hide();
+  			$('.hulk-still').show();
+  		}
+
+  	});
+
+
+
+
+
+
+
+
+
+
+
 });
+
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
