f71212a39d7ff6d770ed41a8b790d1b9a5e588f6
cgberlin <codyberlin@gmail.com>
2016-07-07 22:25:08 -0700

    shortened jQuery, added functions, reduced lines to 80

--- js/app.js
+++ js/app.js
@@ -1,125 +1,80 @@
+
 $(document).ready(function() {
 
-function hideAllRyuStates() {
-  $('.ryu-cool, .hadouken, .ryu-ready, .ryu-still, .ryu-throwing').css("display", "none");
-} 
-
-function hideAllHulkStates() {
-  $('.hulk-cool, .hulk-still, .hulk-throwing, .hulk-ready, .hulk-hadouken').css('display', 'none')
-}
-
-function showHulkReady() {
-  $('.hulk-ready').show();
-}
-
-function showHulkStill() {
-  $('.hulk-still').show();
-}
-
-function showRyuReady() {
-  $('.ryu-ready').show();
-}
-
-function showRyuStill() {
-  $('.ryu-still').show();
-}
-
-  
-
-
-  $('.ryu').mouseenter(function() {
-    hideAllRyuStates();
-    showRyuReady()
-  })
-  .mouseleave(function() {
-  	hideAllRyuStates()
-  	showRyuStill()
-  })
-  .mousedown(function(){
-  	playHadouken();
-  	hideAllRyuStates();
-  	$('.ryu-throwing').show();
-  	$('.hadouken').finish().show().animate(
-  		{'left':'1020px'},
-  		500,
-  		function(){
-  			$(this).hide();
-  			$(this).css('left', '550px');
-  		}
-  		);
-  })
-  .mouseup(function(){
-  	hideAllRyuStates();
-  	showRyuReady()
-  });
-  $(window).keydown(function (e) {
-  		if (e.keyCode == 88) {
-  			hideAllRyuStates();
-  			$('.ryu-cool').show();
-  		}
-  })
-  .keyup(function (e) {
-  		if (e.keyCode == 88) {
-  			hideAllRyuStates();
-  			showRyuStill()
-  		}
-
-  	});
-// hulk ryu below
-
-
- $('.hulk-ryu').mouseenter(function() {
-    hideAllHulkStates();
-    showHulkReady();
-  })
-  .mouseleave(function() {
-  	hideAllHulkStates();
-  	showHulkStill()
-  })
-  .mousedown(function(){
-  	playHadouken();
-  	hideAllHulkStates();
-  	$('.hulk-throwing').show();
-  	$('.hulk-hadouken').finish().show().animate(
-  		{'right':'900px'},
-  		500,
-  		function(){
-  			$(this).hide();
-  			$(this).css('right', '447px');
-  		}
-  		);
-  })
-  .mouseup(function(){
-  	hideAllHulkStates();
-  	showHulkReady();
-  });
-  $(window).keydown(function (e) {
-  		if (e.keyCode == 88) {
-  			hideAllHulkStates();
-  			$('.hulk-cool').show();
-  		}
-  })
-  .keyup(function (e) {
-  		if (e.keyCode == 88) {
-  			hideAllHulkStates();
-  			showHulkStill()
-  		}
-
-  	});
+  function hidePlayer(player) {
+    $(player + ' div').hide();
+  }
+  function showPlayerState(player, state) {
+    hidePlayer(player);
+    $(player + ' ' + state).show();
+  }
+  function playHadouken(player, hadouken, hadoukenAnimation, fromPos, toPos) {
+    playHadoukenSound();
+    hidePlayer(player);
+    $(hadouken).show();
+    $(hadoukenAnimation).finish().show().animate(
+      {'left': fromPos + 'px'},
+      500,
+      function(){
+        $(this).hide();
+        $(this).css('left', toPos + 'px');
+      }
+    );
+  }
+  function keyXAction(e, player, state) {
+    if (e.keyCode == 88) {
+      showPlayerState(player, state);
+    }
+  }
+  function initAnimationsRyu() {
+    $('.ryu').mouseenter(function () {
+      showPlayerState('.ryu', '.ryu-ready');
+    })
+    .mouseleave(function () {
+      showPlayerState('.ryu', '.ryu-still');
+    })
+    .mousedown(function(){
+      playHadouken('.ryu', '.ryu-throwing', '.hadouken', 1020, 550);
+    })
+    .mouseup(function(){
+      showPlayerState('.ryu', '.ryu-ready');
+    });
+    $(window).keydown(function (e) {
+      keyXAction(e, '.ryu', '.ryu-cool');
+    })
+    .keyup(function (e) {
+      keyXAction(e, '.ryu', '.ryu-still');
+    });
+  }
+  function initAnimationsHulk() {
+    $('.hulk-ryu').mouseenter(function () {
+      showPlayerState('.hulk-ryu', '.hulk-ready');
+    })
+    .mouseleave(function () {
+      showPlayerState('.hulk-ryu', '.hulk-still');
+    })
+    .mousedown(function(){
+      playHadouken('.hulk-ryu', '.hulk-throwing', '.hulk-hadouken', 447, 900);
+    })
+    .mouseup(function(){
+      showPlayerState('.hulk-ryu', '.hulk-ready');
+    });
+    $(window).keydown(function (e) {
+      keyXAction(e, '.hulk-ryu', '.hulk-cool');
+    })
+    .keyup(function (e) {
+      keyXAction(e, '.hulk-ryu', '.hulk-still');
+    });
+  }
 
   $('.streetfighter-logo').fadeIn(2000).delay(1000).fadeOut(2000);
-  
-
   $('.my-name').delay(5000).fadeIn(2000).delay(1000).fadeOut(2000);
-  
+
+  initAnimationsRyu();
+  initAnimationsHulk();
 });
 
-
-
-
-
-
-function playHadouken () {
+function playHadoukenSound () {
   $('#hadouken-sound')[0].volume = 0.5;
   $('#hadouken-sound')[0].load();
   $('#hadouken-sound')[0].play();
